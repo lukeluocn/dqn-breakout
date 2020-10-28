d246a825b7df79db25b071eef96a61de58753d57
lukelook <lukeluocn@outlook.com>
2020-10-28 22:41:24 +0800

    Save model after each eval

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ from utils_memory import ReplayMemory
 GAMMA = 0.99
 GLOBAL_SEED = 0
 MEM_SIZE = 100_000
+SAVE_PREFIX = "./models"
 STACK_SIZE = 4
 
 EPS_START = 1.
@@ -29,6 +30,7 @@ EVALUATE_FREQ = 100_000
 rand = random.Random()
 rand.seed(GLOBAL_SEED)
 new_seed = lambda: rand.randint(0, 1000_000)
+os.mkdir(SAVE_PREFIX)
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 env = MyEnv(device, new_seed())
@@ -71,10 +73,12 @@ for step in progressive:
     if step % EVALUATE_FREQ == 0:
         avg_reward, frames = env.evaluate(obs_queue, agent)
         with open("rewards.txt", "a") as fp:
-            fp.write(f"{step:8d} {avg_reward:.1f}\n")
-        prefix = f"eval_{step // EVALUATE_FREQ}"
+            fp.write(f"{step//EVALUATE_FREQ:3d} {step:8d} {avg_reward:.1f}\n")
+        prefix = f"eval_{step//EVALUATE_FREQ:03d}"
         os.mkdir(prefix)
         for ind, frame in enumerate(frames):
             with open(os.path.join(prefix, f"{ind:06d}.png"), "wb") as fp:
                 frame.save(fp, format="png")
+        agent.save(os.path.join(
+            SAVE_PREFIX, f"model_{step//EVALUATE_FREQ:03d}"))
         done = True

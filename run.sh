957ab4c03122a1f4ff1e271d61ba8f6fb3fc8659
lukelook <lukeluocn@outlook.com>
2020-10-28 22:50:33 +0800

    Add caveat on removing the models dir

--- run.sh
+++ run.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+if [ -d models ]; then
+    read -p "Would remove the existing models/ dir. Proceed on? [y/n] " ins
+    if [[ ${ins} == "y" ]]; then
+        rm -r models
+    else
+        exit
+    fi
+fi
 rm -r eval_*
 
 if [ -z ${CUDA_VISIBLE_DEVICES} ]; then
